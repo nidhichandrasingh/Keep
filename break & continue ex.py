332ea04fb72c7274401b9ae6aa8ca2a04e0ef444
nidhichandrasingh <83943807+nidhichandrasingh@users.noreply.github.com>
2022-09-14 17:19:00 +0530

    Delete break & continue ex.py

--- break & continue ex.py
+++
@@ -1,9 +0,0 @@
-while(True):
-    i=int(input("enter any choice \n"))
-    if i<100:
-          print("try angain later \n")
-          continue
-    if i>= 100:
-          print ("congrats u got the number \n")
-          break
-          
